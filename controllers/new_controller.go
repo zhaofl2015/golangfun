d2f0a517784ef3a5807a98bfc1812527bbc695e4
zhaofl2015 <zhaofl2015@163.com>
2017-02-14 18:20:32 +0800

    mongo

--- controllers/new_controller.go
+++ controllers/new_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"news/Blog"
+
 	"github.com/astaxie/beego"
 )
 
@@ -9,7 +11,11 @@ type NewsController struct {
 }
 
 func (c *NewsController) Get() {
-	c.Data["Title"] = "A light is on for ever"
-	c.Data["Content"] = "As we know, a light is turned off for resource reasons, but today China's handsomest guy has a light is on for ever"
+	blog := Blog.get_newest_blog()
+
+	c.Data["Title"] = blog.title
+	c.Data["Content"] = blog.content
+	//	c.Data["Title"] = "A light is on for ever"
+	//	c.Data["Content"] = "As we know, a light is turned off for resource reasons, but today China's handsomest guy has a light is on for ever"
 	c.TplName = "news.html"
 }
