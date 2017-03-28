a8d78e341b5723ab3a26da6bcab3d89f31a9009e
zhaofl2015 <zhaofl2015@163.com>
2017-03-28 23:34:36 +0800

    vue

---
+++ my-webpack-demo/src/components/Bottom.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="layout-copy">
+      2016-2017 &copy; fleago@163.com
+  </div>
+</template>
+<script>
+    export default {
+      name: 'bottom'
+    }
+</script>
+
+<style scoped>
+  .layout-copy{
+      text-align: center;
+      padding: 10px 0 20px;
+      color: #9ea7b4;
+  }
+</style>
